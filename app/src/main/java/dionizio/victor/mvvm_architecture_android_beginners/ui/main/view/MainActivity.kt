b92a653b69e3cbb48952451c4f9495f030e466f0
Victor Dionizio <victor.dionizio28@gmail.com>
2022-11-23 20:57:29 -0300

    Configurando ViewModel

--- app/src/main/java/dionizio/victor/mvvm_architecture_android_beginners/ui/main/view/MainActivity.kt
+++ app/src/main/java/dionizio/victor/mvvm_architecture_android_beginners/ui/main/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package dionizio.victor.mvvm_architecture_android_beginners
+package dionizio.victor.mvvm_architecture_android_beginners.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dionizio.victor.mvvm_architecture_android_beginners.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/dionizio/victor/mvvm_architecture_android_beginners/ui/main/viewmodel/MainViewModel.kt
@@ -0,0 +1,43 @@
+package dionizio.victor.mvvm_architecture_android_beginners.ui.main.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dionizio.victor.mvvm_architecture_android_beginners.data.model.User
+import dionizio.victor.mvvm_architecture_android_beginners.data.repository.MainRepository
+import dionizio.victor.mvvm_architecture_android_beginners.utils.Resource
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+
+class MainViewModel(private val mainRepository: MainRepository) : ViewModel(){
+    private val users = MutableLiveData<Resource<List<User>>>()
+    private val compositeDisposable = CompositeDisposable()
+
+    init {
+        fetchUsers()
+    }
+
+    private fun fetchUsers() {
+        users.postValue(Resource.loading(null))
+        compositeDisposable.add(
+            mainRepository.getUsers()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ userList ->
+                    users.postValue(Resource.success(userList))
+                }, { throwable ->
+                    users.postValue(Resource.error("Something Went Wrong", null))
+                })
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+
+    fun getUsers(): LiveData<Resource<List<User>>> {
+        return users
+    }
+}
